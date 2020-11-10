3e1aec3af71f49cef479cd60b99f3ec46e1050e7
likwid23 <likwid2@hotmail.com>
2020-11-09 22:24:01 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
